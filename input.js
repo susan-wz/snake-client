d8a4be96982643023b8e1780a06a8cac573ba9c8
Susan Zhang <susanwenzhang@gmail.com>
2019-12-04 20:49:05 +0000

    added commands and connected things, still confused

--- input.js
+++ input.js
@@ -2,11 +2,23 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   const handleUserInput = (key) => {
     if (key === '\u0003') {
       process.exit();
+    } else if (key === "w") {
+      conn.write('Move: up');
+    } else if (key === "a") {
+      conn.write('Move: left');
+    } else if (key === "s") {
+      conn.write('Move: down');
+    } else if (key === "d") {
+      conn.write('Move: right');
     }
   };
   stdin.setRawMode(true);
