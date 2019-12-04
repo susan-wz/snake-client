b22ead9e53b8fb9d43d9c7c4f48e5a46703e1ff0
Susan Zhang <susanwenzhang@gmail.com>
2019-12-04 19:17:28 +0000

    put connect function in the client file and added message to server on connection

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '172.46.2.204',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  conn.setEncoding('utf8'); 
+
+  conn.on('connect', (callback) => {
+    console.log("Successfully connected to game server");
+  })
+
+  conn.on('connect', () => {
+    conn.write('Name: SWZ');
+  })
+
+  return conn;
+}
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -3,19 +3,7 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '172.46.2.204',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  conn.setEncoding('utf8'); 
 
-  return conn;
-}
-
+const {connect} = require('./client');
 console.log('Connecting ...');
 connect();
